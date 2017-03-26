b4721539fdd2f8b75a6854f02f680e122f1b3598
Jay Lingelbach <Jay.Lingelbach@gmail.com>
2017-03-26 18:17:04 -0500

    Load and LoadList components added

--- app/components/LoadDataApp.jsx
+++ app/components/LoadDataApp.jsx
@@ -1,10 +1,43 @@
 var React = require('react');
+var LoadList = require('LoadList');
+
 
 var LoadDataApp = React.createClass({
+
+  getInitialState: function() {
+    return {
+      loads: [
+        {
+          id: 1,
+          powderMan: 'Vihtavouori ',
+          powder: 'N140 ',
+          charge: '42.8gr ',
+          primer: 'CCI 200 ',
+          bulletDes: 'Lapua D166 ',
+          bulletWei: '200gr',
+          oal: '2.99 '
+        },
+        {
+          id: 2,
+          powderMan: 'IMR ',
+          powder: '4895 ',
+          charge: '32gr ',
+          primer: ' CCI 200 ',
+          bulletDes: 'Lyman 311299 ',
+          bulletWei: '200gr',
+          oal: ' 3.24 '
+        }
+
+      ]
+    };
+  },
+
   render: function() {
+    var {loads} = this.state;
+
     return(
         <div>
-        LoadDataApp.jsx
+          <LoadList loads= {loads}/>
         </div>
     )
   }
